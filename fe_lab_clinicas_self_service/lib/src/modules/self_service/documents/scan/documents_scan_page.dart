57f97143e1e68c204f062d67291ce2213f0e45f9
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-02-12 15:17:03 -0300

    personalizar camera

--- fe_lab_clinicas_self_service/lib/src/modules/self_service/documents/scan/documents_scan_page.dart
+++ fe_lab_clinicas_self_service/lib/src/modules/self_service/documents/scan/documents_scan_page.dart
@@ -1,4 +1,5 @@
 import 'package:camera/camera.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/self_service/widget/lab_clinicas_self_service_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -42,12 +43,12 @@ class _DocumentsScanPageState extends State<DocumentsScanPage> {
               child: Column(
                 children: [
                   Image.asset('assets/images/cam_icon.png'),
-                  const SizedBox(height: 24),
+                  const SizedBox(height: 15),
                   const Text(
                     'TIRAR A FOTO AGORA',
                     style: LabClinicasTheme.titleSmallStyle,
                   ),
-                  const SizedBox(height: 32),
+                  const SizedBox(height: 15),
                   const Text(
                     'Posicione o documento dentro do quadrado abaixo e aperte o botão para tirar a foto',
                     textAlign: TextAlign.center,
@@ -56,7 +57,7 @@ class _DocumentsScanPageState extends State<DocumentsScanPage> {
                         fontWeight: FontWeight.w500,
                         color: LabClinicasTheme.blueColor),
                   ),
-                  const SizedBox(height: 24),
+                  const SizedBox(height: 20),
                   FutureBuilder(
                     future: _cameraController.initialize(),
                     builder: (context, snapshot) {
@@ -71,7 +72,22 @@ class _DocumentsScanPageState extends State<DocumentsScanPage> {
                             connectionState: ConnectionState.done
                           ):
                           if (_cameraController.value.isInitialized) {
-                            return CameraPreview(_cameraController);
+                            return ClipRRect(
+                                borderRadius: BorderRadius.circular(16),
+                                child: SizedBox(
+                                  width: sizeOf.width * .43,
+                                  child: CameraPreview(
+                                    _cameraController,
+                                    child: DottedBorder(
+                                        borderType: BorderType.RRect,
+                                        strokeWidth: 4,
+                                        strokeCap: StrokeCap.square,
+                                        color: LabClinicasTheme.orangeColor,
+                                        dashPattern: const [1, 10, 1, 3],
+                                        radius: const Radius.circular(16),
+                                        child: const SizedBox.expand()),
+                                  ),
+                                ));
                           }
                       }
 
@@ -79,7 +95,15 @@ class _DocumentsScanPageState extends State<DocumentsScanPage> {
                         child: Text('Erro ao carregar camera'),
                       );
                     },
-                  )
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                      width: sizeOf.width * .8,
+                      height: 48,
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text('TIRAR FOTO')))
                 ],
               )),
         ),
