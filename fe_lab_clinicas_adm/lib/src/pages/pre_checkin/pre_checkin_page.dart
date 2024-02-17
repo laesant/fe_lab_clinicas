f8ecb9797695913d7bf674d22610d1687f86c4a3
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-02-16 23:33:59 -0300

    construção da tela de pre-checkin

--- fe_lab_clinicas_adm/lib/src/pages/pre_checkin/pre_checkin_page.dart
+++ fe_lab_clinicas_adm/lib/src/pages/pre_checkin/pre_checkin_page.dart
@@ -1,4 +1,8 @@
+import 'package:fe_lab_clinicas_adm/src/shared/data_item.dart';
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class PreCheckinPage extends StatelessWidget {
   const PreCheckinPage({super.key});
@@ -6,10 +10,113 @@ class PreCheckinPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      appBar: LabClinicasAppBar(),
+      body: SingleChildScrollView(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            width: MediaQuery.sizeOf(context).width * .5,
+            margin: const EdgeInsets.only(top: 56),
+            padding: const EdgeInsets.all(40),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(
+                color: LabClinicasTheme.orangeColor,
+              ),
+            ),
+            child: Column(
+              children: [
+                Image.asset('assets/images/patient_avatar.png'),
+                const SizedBox(height: 16),
+                const Text(
+                  'A senha chamada foi',
+                  style: LabClinicasTheme.titleSmallStyle,
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  width: 218,
+                  alignment: Alignment.center,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                  decoration: BoxDecoration(
+                    color: LabClinicasTheme.orangeColor,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Text(
+                    'Laenio Marques',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 48),
+                DataItem(
+                  label: 'Nome Paciente',
+                  value: 'Laenio Marques',
+                  padding: EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'Email',
+                  value: 'Laenio Marques',
+                  padding: EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'Telefone de contato',
+                  value: 'Laenio Marques',
+                  padding: EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'CPF',
+                  value: 'Laenio Marques',
+                  padding: EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'CEP',
+                  value: 'Laenio Marques',
+                  padding: EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'Endereço',
+                  value: 'Laenio Marques',
+                  padding: EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'Responsável',
+                  value: 'Laenio Marques',
+                  padding: EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'Documento de Identificação',
+                  value: 'Laenio Marques',
+                  padding: EdgeInsets.only(bottom: 24),
+                ),
+                const SizedBox(height: 48),
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                              fixedSize: const Size.fromHeight(48)),
+                          onPressed: () {},
+                          child: Text('CHAMAR OUTRA SENHA')),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                fixedSize: const Size.fromHeight(48)),
+                            onPressed: () {},
+                            child: Text('ATENDER')))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
---
+++ fe_lab_clinicas_adm/lib/src/shared/data_item.dart
@@ -0,0 +1,48 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:flutter/material.dart';
+
+class DataItem extends StatelessWidget {
+  const DataItem({
+    super.key,
+    required this.label,
+    required this.value,
+    this.padding,
+  });
+
+  final String label;
+  final String value;
+  final EdgeInsetsGeometry? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget widget = Row(
+      children: [
+        Text(
+          '$label: ',
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            color: LabClinicasTheme.blueColor,
+          ),
+        ),
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            color: LabClinicasTheme.orangeColor,
+          ),
+        ),
+      ],
+    );
+
+    if (padding != null) {
+      widget = Padding(
+        padding: padding!,
+        child: widget,
+      );
+    }
+
+    return widget;
+  }
+}
